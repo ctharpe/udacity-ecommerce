df44e8894b21e9ecfa8b0700555cf83a0a9935c5
Chris Tharpe <christopher.tharpe@daimler.com>
2021-01-03 18:44:16 -0800

    Testing additions. Added mockito dependency to pom.xml, added TestUtils and UserControllerTest.

---
+++ projects/P04-eCommerce Application/starter_code/src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,52 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+    private UserController userController;
+    private UserRepository userRepository = mock(UserRepository.class);
+    private CartRepository cartRepository = mock(CartRepository.class);
+    private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+
+    @Before
+    public void setup () {
+        userController = new UserController();
+        TestUtils.injectObjects(userController, "userRepository", userRepository);
+        TestUtils.injectObjects(userController, "cartRepository", cartRepository);
+        TestUtils.injectObjects(userController, "bCryptPasswordEncoder", encoder );
+    }
+
+    @Test
+    public void create_user_happy_path() {
+        when(encoder.encode("testPassword")).thenReturn("thisIsHashed");
+        CreateUserRequest userRequest = new CreateUserRequest();
+        userRequest.setUsername("test");
+        userRequest.setPassword("testPassword");
+        userRequest.setConfirmPassword("testPassword");
+
+        final ResponseEntity<User> response = userController.createUser(userRequest);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        User user = response.getBody();
+        assertNotNull(user);
+        assertEquals(0, user.getId());
+        assertEquals("test", user.getUsername());
+        assertEquals("thisIsHashed", user.getPassword());
+
+    }
+}
